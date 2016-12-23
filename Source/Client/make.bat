1e526fdbdf82860e13970524ea55382dc932560c
Dylan Yaga <pyroflyx@gmail.com>
2016-12-23 09:45:50 -0500

    adding more cleanup
    Removing more files after build to keep directory clean.

--- Source/Client/make.bat
+++ Source/Client/make.bat
@@ -17,21 +17,24 @@ echo Compiling RES
 %BINPATH%\brcc32 merc.rc
 echo Linking
 %BINPATH%\ilink32 /aa -L%LIBPATH% -Llpng -Lzlib -L%LIBPATH%\psdk %LIBPATH%\c0w32.obj dd.obj engine.obj main.obj inter.obj socket.obj sound.obj conv.obj options.obj,merc.exe,,cw32mt.lib import32.lib zlib.lib libpng.lib ddraw.lib dsound.lib,merc.def,merc.res
-echo Removing .objs
-del *.obj
-echo Finished
 
+echo Cleaning Up
+del *.obj
+del merc.ilc
+del merc.ild
+del merc.ilf
+del merc.ils
+del merc.map
+del merc.RES
+del merc.tds
 
 echo Packaging Client
-
 mkdir %OUTPATH%
 mkdir %OUTPATH%\gfx
 mkdir %OUTPATH%\sfx
-
-copy  merc.exe %OUTPATH%
+move  merc.exe %OUTPATH%
 copy "..\..\Resources\Packaged Graphics\*.dat" %OUTPATH%
 copy "..\..\Resources\Packaged Graphics\*.idx" %OUTPATH%
 copy "..\..\Resources\Sound\sfx\*.*" %OUTPATH%\sfx
-
-
+echo Finished
 pause

e74be961ec6c8346287a6f0b3f49052cbc9a4f3f
Dylan Yaga <pyroflyx@gmail.com>
2016-12-23 09:28:30 -0500

    Missed a few paths.

--- Source/Client/make.bat
+++ Source/Client/make.bat
@@ -1,6 +1,7 @@
 @echo off
 SET BINPATH=C:\Borland\BCC55\Bin
 SET INCPATH=C:\Borland\BCC55\Include
+SET LIBPATH=C:\Borland\BCC55\Lib
 echo Compiling Source
 %BINPATH%\bcc32.exe -I%INCPATH% -Izlib -O2 -5 -d -ff -k- -OS -Q -X -W -WM -c dd.c -o dd.obj
 %BINPATH%\bcc32.exe -I%INCPATH% -Izlib -O2 -5 -d -ff -k- -OS -Q -X -W -WM -c engine.c -o engine.obj
@@ -13,7 +14,7 @@ echo Compiling Source
 echo Compiling RES
 %BINPATH%\brcc32 merc.rc
 echo Linking
-%BINPATH%\ilink32 /aa -LC:\bcc\lib -Llpng -Lzlib -LC:\bcc\lib\psdk C:\bcc\lib\c0w32.obj dd.obj engine.obj main.obj inter.obj socket.obj sound.obj conv.obj options.obj,merc.exe,,cw32mt.lib import32.lib zlib.lib libpng.lib ddraw.lib dsound.lib,merc.def,merc.res
+%BINPATH%\ilink32 /aa -L%LIBPATH% -Llpng -Lzlib -L%LIBPATH%\psdk %LIBPATH%\c0w32.obj dd.obj engine.obj main.obj inter.obj socket.obj sound.obj conv.obj options.obj,merc.exe,,cw32mt.lib import32.lib zlib.lib libpng.lib ddraw.lib dsound.lib,merc.def,merc.res
 echo Removing .objs
 del *.obj
 echo Finished
